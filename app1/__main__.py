bbc090518342c45cac1f651784999a6396b60ebb
s <e>
2024-08-19 03:20:43 +0000

    test

--- app1/__main__.py
+++ app1/__main__.py
@@ -20,18 +20,31 @@ def input_(value: str) -> e.Effect[Console, t.Never, str]:
     return console.input(value)
 
 
-def app() -> e.Effect[Console, t.Never, int]:
+@e.throws(ZeroDivisionError)
+def divide() -> e.Effect[Console, t.Never, None]:
     "App."
     yield from print_("start up")
+    first = yield from input_("what is the first number? ")
+    second = yield from input_("what is the first number? ")
+    ans = str(float(first) / float(second))
+    yield from print_(f"{first} / {second} = {ans}")
     yield from print_("ending")
-    a = yield from input_("what is your name?")
-    yield from print_(f"hello {a}")
-    return 0
+
+
+def app() -> e.Effect[Console, t.Never, int]:
+    """App."""
+    yield from print_("start up")
+    result = yield from e.catch(divide)()
+    match result:
+        case ZeroDivisionError():
+            return -1
+        case _:
+            return 0
 
 
 def main() -> int:
     "main function." ""
-    run = Runtime()
+    run: Runtime = Runtime()
     run.use(Console()).run(app())
     return 0
 
